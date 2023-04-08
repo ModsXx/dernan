b50feff56c19914ea196812cd1b871b65f73fd49
ModsXx <118561210+ModsXx@users.noreply.github.com>
2023-04-08 11:57:10 -0500

    Add files via upload

---
+++ assets/preload/stages/uwu.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('house', 'map', 400, 520);
+scaleObject('house', 1.6, 1.5);
+	addLuaSprite('house',false)
+        setLuaSpriteScrollFactor('house', 1, 1);
+
+
+end
